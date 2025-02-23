8a0c91242d973ee94694fcc16fa2e9c8e365c6c7
KenichiroArai <sammyleejunior@gmail.com>
2025-02-23 14:56:36 +0900

    JavadocAppenderToolクラスにテンプレートファイルのパスを追加し、実行メソッドを実装

--- src/main/java/kmg/tool/presentation/JavadocAppenderTool.java
+++ src/main/java/kmg/tool/presentation/JavadocAppenderTool.java
@@ -1,9 +1,65 @@
 package kmg.tool.presentation;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+import kmg.core.infrastructure.types.KmgDelimiterTypes;
+
 /**
  * Javadoc追加ツール
  */
 public class JavadocAppenderTool {
+
+    /** 基準パス */
+    private static final Path BASE_PATH = Paths.get(String.format("src/main/resources/tool/io"));
+
+    /** テンプレートファイルパス */
+    // TODO KenichiroArai 2025/02/23 自動設定
+    private static final Path TEMPLATE_PATH
+        = Paths.get(JavadocAppenderTool.BASE_PATH.toString(), "template/JavadocAppenderTool.txt");
+
+    /**
+     * 実行する<br>
+     *
+     * @author KenichiroArai
+     *
+     * @sine 0.1.0
+     *
+     * @version 0.1.0
+     *
+     * @return TRUE：成功、FLASE：失敗
+     *
+     * @throws FileNotFoundException
+     *                               ファイルが存在しない例外
+     * @throws IOException
+     *                               入出力例外
+     */
+    @SuppressWarnings("static-method")
+    public Boolean run() throws FileNotFoundException, IOException {
+
+        final Boolean result = Boolean.FALSE;
+
+        /* テンプレートの取得 */
+        String template = null;
+
+        try {
+
+            template = Files.readAllLines(JavadocAppenderTool.TEMPLATE_PATH).stream()
+                .collect(Collectors.joining(KmgDelimiterTypes.LINE_SEPARATOR.get()));
+
+        } catch (final IOException e) {
+
+            throw e;
+
+        }
+
+        return result;
+
+    }
 
     /**
      * メインメソッド
@@ -12,7 +68,29 @@ public class JavadocAppenderTool {
      *             引数
      */
     public static void main(final String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+
+        final Class<JavadocAppenderTool> clasz = JavadocAppenderTool.class;
+
+        try {
+
+            final JavadocAppenderTool main = new JavadocAppenderTool();
+
+            if (main.run()) {
+
+                System.out.println(String.format("%s：失敗", clasz.toString()));
+
+            }
+
+        } catch (final Exception e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            System.out.println(String.format("%s：成功", clasz.toString()));
+            System.out.println(String.format("%s：終了", clasz.toString()));
+
+        }
 
     }
 
