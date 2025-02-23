a2fc74b60346b4adf9c499a7ac733645209009e3
KenichiroArai <sammyleejunior@gmail.com>
2025-02-23 14:51:04 +0900

    JavadocAppenderToolクラスを追加し、Javadoc追加ツールの基本構造を実装

---
+++ src/main/java/kmg/tool/presentation/JavadocAppenderTool.java
@@ -0,0 +1,19 @@
+package kmg.tool.presentation;
+
+/**
+ * Javadoc追加ツール
+ */
+public class JavadocAppenderTool {
+
+    /**
+     * メインメソッド
+     *
+     * @param args
+     *             引数
+     */
+    public static void main(final String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+}
