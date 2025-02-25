8e6d18732303cc310ae1cec8c9118e7700ea7cc5
KenichiroArai <sammyleejunior@gmail.com>
2025-02-25 18:14:22 +0900

    JavadocLineRemoverToolに入力ファイルからパスと行番号のマップを取得するメソッドを追加

--- src/main/java/kmg/tool/presentation/ui/cli/JavadocLineRemoverTool.java
+++ src/main/java/kmg/tool/presentation/ui/cli/JavadocLineRemoverTool.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import kmg.core.domain.service.KmgPfaMeasService;
 import kmg.core.domain.service.impl.KmgPfaMeasServiceImpl;
+import kmg.core.infrastructure.types.KmgDelimiterTypes;
 
 /**
  * Javadoc行削除ツール
@@ -46,6 +49,10 @@ public class JavadocLineRemoverTool {
 
         final boolean result = true;
 
+        /* 入力からパスと行番号のマップ（ファイルパスと行番号のリスト、リストは行番号の降順）を取得 */
+        final Map<Path, List<Integer>> inputMap = JavadocLineRemoverTool.getInputMap();
+        System.out.println(inputMap.toString());
+
         /* 対象のJavaファイルを取得 */
         final List<Path> javaFileList;
 
@@ -70,6 +77,51 @@ public class JavadocLineRemoverTool {
         System.out.println(String.format("lineCount: %d", lineCount));
 
         return result;
+
+    }
+
+    /**
+     * 入力ファイルからパスと行番号のマップを取得する
+     *
+     * @return パスと行番号のマップ
+     *
+     * @throws IOException
+     *                     入出力例外
+     */
+    private static Map<Path, List<Integer>> getInputMap() throws IOException {
+
+        try (final Stream<String> stream = Files.lines(JavadocLineRemoverTool.INPUT_PATH)) {
+
+            final Map<Path, List<Integer>> result = stream.filter(line -> line.contains(".java:")).map(line -> {
+
+                // ファイルパスと行番号を抽出
+                final String[] parts = KmgDelimiterTypes.COLON.split(line);
+
+                if (parts.length < 3) {
+
+                    return null;
+
+                }
+
+                try {
+
+                    final Path path       = Paths.get(parts[1].trim());
+                    final int  lineNumber = Integer.parseInt(parts[2].trim());
+                    return new SimpleEntry<>(path, lineNumber);
+
+                } catch (@SuppressWarnings("unused") final Exception e) {
+
+                    // 処理なし
+                }
+
+                return null;
+
+            }).filter(entry -> entry != null).collect(
+                Collectors.groupingBy(Map.Entry::getKey, Collectors.mapping(Map.Entry::getValue, Collectors.toList())));
+
+            return result;
+
+        }
 
     }
 
