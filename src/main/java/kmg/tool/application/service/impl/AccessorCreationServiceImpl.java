142e09060b2d789ca3f105c67a8716951bfd12ea
KenichiroArai <sammyleejunior@gmail.com>
2025-03-07 00:39:01 +0900

    AccessorCreationServiceImplにCSV処理機能を追加し、DynamicTemplateConversionServiceImplの不要なコードを削除

--- src/main/java/kmg/tool/application/service/impl/AccessorCreationServiceImpl.java
+++ src/main/java/kmg/tool/application/service/impl/AccessorCreationServiceImpl.java
@@ -1,8 +1,20 @@
 package kmg.tool.application.service.impl;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Service;
 
+import kmg.core.infrastructure.type.KmgString;
 import kmg.tool.application.service.AccessorCreationService;
+import kmg.tool.domain.types.KmgToolGenMessageTypes;
 import kmg.tool.infrastructure.exception.KmgToolException;
 
 /**
@@ -26,6 +38,64 @@ public class AccessorCreationServiceImpl extends DynamicTemplateConversionServic
     public boolean process() throws KmgToolException {
 
         boolean result = false;
+
+        /* 入力から出力の処理 */
+        final List<List<String>> csv = new ArrayList<>();
+
+        final Path csvPath = Path.of("");
+
+        try (final BufferedReader brInput = Files.newBufferedReader(this.getInputPath());
+            final BufferedWriter bw = Files.newBufferedWriter(csvPath);) {
+
+            String line = null;
+
+            while ((line = brInput.readLine()) != null) {
+
+                // final String[] keyArrays = columnMappings.values().toArray(new String[0]);
+
+                line = line.replace("final", KmgString.EMPTY);
+                line = line.replace("static", KmgString.EMPTY);
+                final Pattern patternComment = Pattern.compile("/\\*\\* (\\S+)");
+                final Matcher matcherComment = patternComment.matcher(line);
+
+                if (matcherComment.find()) {
+
+                    final List<String> csvLine = new ArrayList<>();
+                    csvLine.add(matcherComment.group(1));
+                    csv.add(csvLine);
+
+                    continue;
+
+                }
+
+                final Pattern patternSrc = Pattern.compile("private\\s+((\\w|\\[\\]|<|>)+)\\s+(\\w+);");
+                final Matcher matcherSrc = patternSrc.matcher(line);
+
+                if (!matcherSrc.find()) {
+
+                    continue;
+
+                }
+
+                final List<String> csvLine = csv.getLast();
+                csvLine.add(matcherSrc.group(1));
+                csvLine.add(matcherSrc.group(3));
+                csvLine.add(matcherSrc.group(3).substring(0, 1).toUpperCase() + matcherSrc.group(3).substring(1));
+
+                bw.write(String.join(",", csvLine));
+                bw.write(System.lineSeparator());
+
+            }
+
+            result = true;
+
+        } catch (final IOException e) {
+
+            // TODO KenichiroArai 2025/03/06 メッセージ
+            final KmgToolGenMessageTypes msgType = KmgToolGenMessageTypes.NONE;
+            throw new KmgToolException(msgType, e);
+
+        }
 
         result = super.process();
 
--- src/main/java/kmg/tool/application/service/impl/DynamicTemplateConversionServiceImpl.java
+++ src/main/java/kmg/tool/application/service/impl/DynamicTemplateConversionServiceImpl.java
@@ -1,19 +1,14 @@
 package kmg.tool.application.service.impl;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.yaml.snakeyaml.Yaml;
 
-import kmg.core.infrastructure.type.KmgString;
 import kmg.tool.application.service.DynamicTemplateConversionService;
 import kmg.tool.domain.types.KmgToolGenMessageTypes;
 import kmg.tool.infrastructure.exception.KmgToolException;
@@ -166,57 +161,7 @@ public class DynamicTemplateConversionServiceImpl implements DynamicTemplateConv
 
         template = (String) yamlData.get("template");
 
-        /* 入力から出力の処理 */
-        try (final BufferedReader brInput = Files.newBufferedReader(this.inputPath);
-            final BufferedWriter bw = Files.newBufferedWriter(this.outputPath);) {
-
-            String output = template;
-            String line   = null;
-
-            while ((line = brInput.readLine()) != null) {
-
-                final String[] keyArrays = columnMappings.values().toArray(new String[0]);
-
-                line = line.replace("final", KmgString.EMPTY);
-                line = line.replace("static", KmgString.EMPTY);
-                final Pattern patternComment = Pattern.compile("/\\*\\* (\\S+)");
-                final Matcher matcherComment = patternComment.matcher(line);
-
-                if (matcherComment.find()) {
-
-                    output = output.replace(keyArrays[0], matcherComment.group(1));
-                    continue;
-
-                }
-
-                final Pattern patternSrc = Pattern.compile("private\\s+((\\w|\\[\\]|<|>)+)\\s+(\\w+);");
-                final Matcher matcherSrc = patternSrc.matcher(line);
-
-                if (!matcherSrc.find()) {
-
-                    continue;
-
-                }
-
-                output = output.replace(keyArrays[1], matcherSrc.group(3));
-                output = output.replace(keyArrays[2], matcherSrc.group(1));
-                output = output.replace("{Item}",
-                    matcherSrc.group(3).substring(0, 1).toUpperCase() + matcherSrc.group(3).substring(1));
-                bw.write(output);
-                bw.write(System.lineSeparator());
-                output = template;
-
-            }
-
-            result = true;
-
-        } catch (final IOException e) {
-
-            // TODO KenichiroArai 2025/03/06 メッセージ
-            final KmgToolGenMessageTypes msgType = KmgToolGenMessageTypes.NONE;
-            throw new KmgToolException(msgType, e);
-
-        }
+        result = true;
 
         return result;
 
