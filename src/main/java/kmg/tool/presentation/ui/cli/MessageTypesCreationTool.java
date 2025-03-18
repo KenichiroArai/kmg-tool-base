bf340bfb953cfbab7c1c167d1fb3aae60353050a
KenichiroArai <sammyleejunior@gmail.com>
2025-03-18 22:44:25 +0900

    メッセージの種類作成ツールとテンプレートファイルを追加し、入力データから出力を生成する機能を実装

--- src/main/java/kmg/tool/presentation/ui/cli/MessageTypesCreationTool.java
+++ src/main/java/kmg/tool/presentation/ui/cli/MessageTypesCreationTool.java
@@ -13,7 +13,7 @@ import kmg.core.infrastructure.type.KmgString;
 import kmg.core.infrastructure.types.KmgDelimiterTypes;
 
 /**
- * 名称・メッセージ作成ツール
+ * メッセージの種類作成ツール
  *
  * @author KenichiroArai
  *
@@ -21,20 +21,20 @@ import kmg.core.infrastructure.types.KmgDelimiterTypes;
  *
  * @version 1.0.0
  */
-public class NameMsgCreationTool {
+public class MessageTypesCreationTool {
 
     /** 基準パス */
     private static final Path BASE_PATH = Paths.get(String.format("src/main/resources/tool/io"));
 
     /** テンプレートファイルパス */
     private static final Path TEMPLATE_PATH
-        = Paths.get(NameMsgCreationTool.BASE_PATH.toString(), "template/kmgTlNameMsgCreationTool.txt");
+        = Paths.get(MessageTypesCreationTool.BASE_PATH.toString(), "template/MessageTypesCreationTool.yml");
 
     /** 入力ファイルパス */
-    private static final Path INPUT_PATH = Paths.get(NameMsgCreationTool.BASE_PATH.toString(), "input.txt");
+    private static final Path INPUT_PATH = Paths.get(MessageTypesCreationTool.BASE_PATH.toString(), "input.txt");
 
     /** 出力ファイルパス */
-    private static final Path OUTPUT_PATH = Paths.get(NameMsgCreationTool.BASE_PATH.toString(), "output.txt");
+    private static final Path OUTPUT_PATH = Paths.get(MessageTypesCreationTool.BASE_PATH.toString(), "output.txt");
 
     /** パラメータ：コメント */
     private static final String PARAM_COMMENT = "$comment";
@@ -62,11 +62,11 @@ public class NameMsgCreationTool {
      */
     public static void main(final String[] args) {
 
-        final Class<NameMsgCreationTool> clasz = NameMsgCreationTool.class;
+        final Class<MessageTypesCreationTool> clasz = MessageTypesCreationTool.class;
 
         try {
 
-            final NameMsgCreationTool main = new NameMsgCreationTool();
+            final MessageTypesCreationTool main = new MessageTypesCreationTool();
 
             if (main.run()) {
 
@@ -113,7 +113,7 @@ public class NameMsgCreationTool {
 
         try {
 
-            template = Files.readAllLines(NameMsgCreationTool.TEMPLATE_PATH).stream()
+            template = Files.readAllLines(MessageTypesCreationTool.TEMPLATE_PATH).stream()
                 .collect(Collectors.joining(KmgDelimiterTypes.LINE_SEPARATOR.get()));
 
         } catch (final IOException e) {
@@ -123,8 +123,8 @@ public class NameMsgCreationTool {
         }
 
         /* 入力から出力の処理 */
-        try (final BufferedReader brInput = Files.newBufferedReader(NameMsgCreationTool.INPUT_PATH);
-            final BufferedWriter bw = Files.newBufferedWriter(NameMsgCreationTool.OUTPUT_PATH);) {
+        try (final BufferedReader brInput = Files.newBufferedReader(MessageTypesCreationTool.INPUT_PATH);
+            final BufferedWriter bw = Files.newBufferedWriter(MessageTypesCreationTool.OUTPUT_PATH);) {
 
             String line;
 
@@ -140,10 +140,10 @@ public class NameMsgCreationTool {
 
                 /* 変換処理 */
                 String output = template;
-                output = output.replace(NameMsgCreationTool.PARAM_COMMENT, nameData.toString()); // コメント
-                output = output.replace(NameMsgCreationTool.PARAM_KEY, idData.toString()); // キー
-                output = output.replace(NameMsgCreationTool.PARAM_NAME, nameData.toString()); // 名称
-                output = output.replace(NameMsgCreationTool.PARAM_VALUE, idData.toString()); // 値
+                output = output.replace(MessageTypesCreationTool.PARAM_COMMENT, nameData.toString()); // コメント
+                output = output.replace(MessageTypesCreationTool.PARAM_KEY, idData.toString()); // キー
+                output = output.replace(MessageTypesCreationTool.PARAM_NAME, nameData.toString()); // 名称
+                output = output.replace(MessageTypesCreationTool.PARAM_VALUE, idData.toString()); // 値
 
                 /* 出力 */
                 bw.write(output);
