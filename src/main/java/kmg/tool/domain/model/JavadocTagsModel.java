415c9e32661ca6e397ef162fa7bbe692dae9cee7
KenichiroArai <sammyleejunior@gmail.com>
2025-04-03 01:14:11 +0900

    Javadocタグモデルとその実装を新規作成し、関連するメソッドを追加

---
+++ src/main/java/kmg/tool/domain/model/JavadocTagsModel.java
@@ -0,0 +1,35 @@
+package kmg.tool.domain.model;
+
+/**
+ * Javadocタグモデル<br>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+public interface JavadocTagsModel {
+
+    /**
+     * 説明を返す<br>
+     *
+     * @return 説明
+     */
+    String getDescription();
+
+    /**
+     * タグを返す<br>
+     *
+     * @return タグ
+     */
+    String getTag();
+
+    /**
+     * 指定値を返す<br>
+     *
+     * @return 指定値
+     */
+    String getValue();
+
+}
---
+++ src/main/java/kmg/tool/domain/model/impl/JavadocTagModelImpl.java
@@ -0,0 +1,63 @@
+package kmg.tool.domain.model.impl;
+
+import kmg.tool.domain.model.JavadocTagsModel;
+
+/**
+ * Javadocタグモデル<br>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+public class JavadocTagModelImpl implements JavadocTagsModel {
+
+    /** タグ */
+    private String tag;
+
+    /** 指定値 */
+    private String value;
+
+    /** 説明 */
+    private String description;
+
+    /**
+     * 説明を返す<br>
+     *
+     * @return 説明
+     */
+    @Override
+    public String getDescription() {
+
+        final String result = this.description;
+        return result;
+
+    }
+
+    /**
+     * タグを返す<br>
+     *
+     * @return タグ
+     */
+    @Override
+    public String getTag() {
+
+        final String result = this.tag;
+        return result;
+
+    }
+
+    /**
+     * 指定値を返す<br>
+     *
+     * @return 指定値
+     */
+    @Override
+    public String getValue() {
+
+        final String result = this.value;
+        return result;
+
+    }
+}
