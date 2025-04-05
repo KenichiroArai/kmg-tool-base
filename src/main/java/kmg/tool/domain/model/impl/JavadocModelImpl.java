852e7eb03179656882061c9fdfacfd467dfeb5d7
KenichiroArai <sammyleejunior@gmail.com>
2025-04-05 23:39:48 +0900

    JavadocModelImplでJavadocTagsModelImplを使用してJavadocタグモデルのリストを初期化するように変更し、冗長なコードを削除しました

--- src/main/java/kmg/tool/domain/model/impl/JavadocModelImpl.java
+++ src/main/java/kmg/tool/domain/model/impl/JavadocModelImpl.java
@@ -1,19 +1,9 @@
 package kmg.tool.domain.model.impl;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
-import kmg.core.infrastructure.type.KmgString;
-import kmg.core.infrastructure.types.KmgDelimiterTypes;
-import kmg.core.infrastructure.types.KmgJavadocTagTypes;
 import kmg.tool.domain.model.JavadocModel;
 import kmg.tool.domain.model.JavadocTagModel;
-import kmg.tool.domain.model.JavadocTagsModel;
 import kmg.tool.infrastructure.exception.KmgToolException;
 
 /**
@@ -44,48 +34,9 @@ public class JavadocModelImpl implements JavadocModel {
      */
     public JavadocModelImpl(final String sourceJavadoc) throws KmgToolException {
 
-        this.javadocTagModelList = new ArrayList<>();
         this.sourceJavadoc = sourceJavadoc;
-
-        // @タグを抽出する正規表現パターン
-        // グループ1: タグ名
-        // グループ2: 値
-        // グループ3: 説明（オプション）
-        final Pattern compiledTagPattern = JavadocTagsModel.COMPILED_TAG_PATTERN;
-        final Matcher compiledTagMatcher = compiledTagPattern.matcher(sourceJavadoc);
-
-        // TODO KenichiroArai 2025/04/03 デバッグ
-        System.out.println("----- 元のJavadoc -----");
-
-        while (compiledTagMatcher.find()) {
-
-            // TODO KenichiroArai 2025/04/03 ハードコード
-
-            // Javadocタグの対象文字列
-            final String srcPatternStr = compiledTagMatcher.group(0); // 元のパターンに一致する文字列
-            // 改行で分割して2行目の処理を行う
-            final String targetStr = Arrays.stream(KmgDelimiterTypes.REGEX_LINE_SEPARATOR.split(srcPatternStr))
-                .map(line -> line.trim().replaceAll("^\\*$", KmgString.EMPTY)).filter(KmgString::isNotBlank)
-                .collect(Collectors.joining(KmgDelimiterTypes.LINE_SEPARATOR.get()));
-
-            // タグ
-            final KmgJavadocTagTypes tag = KmgJavadocTagTypes.getEnum(compiledTagMatcher.group(1));
-
-            // 指定値
-            final String value = compiledTagMatcher.group(2);
-
-            // 説明取得
-            final String description = Optional.ofNullable(compiledTagMatcher.group(3))
-                .map(s -> s.trim().replaceFirst("^\\*", KmgString.EMPTY)).orElse(KmgString.EMPTY).trim();
-
-            final JavadocTagModel javadocTagMode = new JavadocTagModelImpl(targetStr, tag, value, description);
-            this.javadocTagModelList.add(javadocTagMode);
-
-            // TODO KenichiroArai 2025/04/03 デバッグ
-            System.out.println(String.format("対象文字列: %s, タグ: %s, 指定値: %s, 説明: %s", javadocTagMode.getTargetStr(),
-                javadocTagMode.getTag(), javadocTagMode.getValue(), javadocTagMode.getDescription()));
-
-        }
+        final JavadocTagsModelImpl javadocTagsModel = new JavadocTagsModelImpl(sourceJavadoc);
+        this.javadocTagModelList = javadocTagsModel.getJavadocTagModelList();
 
     }
 
@@ -122,5 +73,4 @@ public class JavadocModelImpl implements JavadocModel {
         return result;
 
     }
-
 }
--- src/main/java/kmg/tool/domain/model/impl/JavadocTagsModelImpl.java
+++ src/main/java/kmg/tool/domain/model/impl/JavadocTagsModelImpl.java
@@ -1,10 +1,19 @@
 package kmg.tool.domain.model.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-import kmg.tool.domain.model.JavadocTagsModel;
+import kmg.core.infrastructure.type.KmgString;
+import kmg.core.infrastructure.types.KmgDelimiterTypes;
+import kmg.core.infrastructure.types.KmgJavadocTagTypes;
 import kmg.tool.domain.model.JavadocTagModel;
+import kmg.tool.domain.model.JavadocTagsModel;
+import kmg.tool.infrastructure.exception.KmgToolException;
 
 /**
  * Javadocタグ一覧情報<br>
@@ -38,6 +47,59 @@ public class JavadocTagsModelImpl implements JavadocTagsModel {
     public JavadocTagsModelImpl(final List<JavadocTagModel> javadocTagModelList) {
 
         this.javadocTagModelList = javadocTagModelList;
+
+    }
+
+    /**
+     * コンストラクタ<br>
+     *
+     * @param sourceJavadoc
+     *                      Javadoc
+     *
+     * @throws KmgToolException
+     *                          KMGツール例外
+     */
+    public JavadocTagsModelImpl(final String sourceJavadoc) throws KmgToolException {
+
+        this.javadocTagModelList = new ArrayList<>();
+
+        // @タグを抽出する正規表現パターン
+        // グループ1: タグ名
+        // グループ2: 値
+        // グループ3: 説明（オプション）
+        final Pattern compiledTagPattern = JavadocTagsModel.COMPILED_TAG_PATTERN;
+        final Matcher compiledTagMatcher = compiledTagPattern.matcher(sourceJavadoc);
+
+        // TODO KenichiroArai 2025/04/03 デバッグ
+        System.out.println("----- 元のJavadoc -----");
+
+        while (compiledTagMatcher.find()) {
+
+            // Javadocタグの対象文字列
+            final String srcPatternStr = compiledTagMatcher.group(0);
+            // 改行で分割して2行目の処理を行う
+            final String targetStr = Arrays.stream(KmgDelimiterTypes.REGEX_LINE_SEPARATOR.split(srcPatternStr))
+                .map(line -> line.trim().replaceAll("^\\*$", KmgString.EMPTY)).filter(KmgString::isNotBlank)
+                .collect(Collectors.joining(KmgDelimiterTypes.LINE_SEPARATOR.get()));
+
+            // タグ
+            final KmgJavadocTagTypes tag = KmgJavadocTagTypes.getEnum(compiledTagMatcher.group(1));
+
+            // 指定値
+            final String value = compiledTagMatcher.group(2);
+
+            // 説明取得
+            final String description = Optional.ofNullable(compiledTagMatcher.group(3))
+                .map(s -> s.trim().replaceFirst("^\\*", KmgString.EMPTY)).orElse(KmgString.EMPTY).trim();
+
+            final JavadocTagModel javadocTagMode = new JavadocTagModelImpl(targetStr, tag, value, description);
+            this.javadocTagModelList.add(javadocTagMode);
+
+            // TODO KenichiroArai 2025/04/03 デバッグ
+            System.out.println(String.format("対象文字列: %s, タグ: %s, 指定値: %s, 説明: %s", javadocTagMode.getTargetStr(),
+                javadocTagMode.getTag(), javadocTagMode.getValue(), javadocTagMode.getDescription()));
+
+        }
 
     }
 
