647b643909c8e69773cfbcc0c6397536689ebb17
KenichiroArai <sammyleejunior@gmail.com>
2025-05-05 00:48:44 +0900

    KmgToolValMessageTypesおよびKmgToolComValMessageTypesインタフェースを新規作成し、バリデーションメッセージの種類を定義しました。可読性と拡張性を向上させるための変更。

---
+++ src/main/java/kmg/tool/domain/types/KmgToolValMessageTypes.java
@@ -0,0 +1,268 @@
+package kmg.tool.domain.types;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import kmg.tool.infrastructure.common.KmgToolComExcMessageTypes;
+import kmg.tool.infrastructure.common.KmgToolComValMessageTypes;
+
+/**
+ * KMGツールバリデーションメッセージの種類<br>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+public enum KmgToolValMessageTypes implements KmgToolComValMessageTypes, KmgToolComExcMessageTypes {
+
+    /* 定義：開始 */
+
+    /**
+     * 指定無し
+     *
+     * @author KenichiroArai
+     *
+     * @since 0.1.0
+     *
+     * @version 0.1.0
+     */
+    NONE("指定無し"),
+
+    /* 定義：終了 */
+
+    ;
+
+    /**
+     * 種類のマップ
+     *
+     * @since 0.1.0
+     */
+    private static final Map<String, KmgToolValMessageTypes> VALUES_MAP = new HashMap<>();
+
+    static {
+
+        /* 種類のマップにプット */
+        for (final KmgToolValMessageTypes type : KmgToolValMessageTypes.values()) {
+
+            KmgToolValMessageTypes.VALUES_MAP.put(type.get(), type);
+
+        }
+
+    }
+
+    /**
+     * 表示名
+     *
+     * @since 0.1.0
+     */
+    private final String displayName;
+
+    /**
+     * メッセージのキー
+     *
+     * @since 0.1.0
+     */
+    private final String key;
+
+    /**
+     * メッセージの値
+     *
+     * @since 0.1.0
+     */
+    private final String value;
+
+    /**
+     * 詳細情報
+     *
+     * @since 0.1.0
+     */
+    private final String detail;
+
+    /**
+     * デフォルトの種類を返す<br>
+     *
+     * @since 0.1.0
+     *
+     * @return デフォルト値
+     */
+    public static KmgToolValMessageTypes getDefault() {
+
+        final KmgToolValMessageTypes result = NONE;
+        return result;
+
+    }
+
+    /**
+     * キーに該当する種類を返す<br>
+     * <p>
+     * 但し、キーが存在しない場合は、指定無し（NONE）を返す。
+     * </p>
+     *
+     * @since 0.1.0
+     *
+     * @param key
+     *            キー
+     *
+     * @return 種類。指定無し（NONE）：キーが存在しない場合。
+     */
+    public static KmgToolValMessageTypes getEnum(final String key) {
+
+        KmgToolValMessageTypes result = KmgToolValMessageTypes.VALUES_MAP.get(key);
+
+        if (result == null) {
+
+            result = NONE;
+
+        }
+        return result;
+
+    }
+
+    /**
+     * 初期値の種類を返す<br>
+     *
+     * @since 0.1.0
+     *
+     * @return 初期値
+     */
+    public static KmgToolValMessageTypes getInitValue() {
+
+        final KmgToolValMessageTypes result = NONE;
+        return result;
+
+    }
+
+    /**
+     * コンストラクタ<br>
+     *
+     * @since 0.1.0
+     *
+     * @param displayName
+     *                    表示名
+     */
+    KmgToolValMessageTypes(final String displayName) {
+
+        this.displayName = displayName;
+        this.key = super.name();
+        this.value = displayName;
+        this.detail = displayName;
+
+    }
+
+    /**
+     * メッセージのキーを返す。<br>
+     *
+     * @since 0.1.0
+     *
+     * @return メッセージのキー
+     *
+     * @see #getKey()
+     */
+    @Override
+    public String get() {
+
+        final String result = this.getKey();
+        return result;
+
+    }
+
+    /**
+     * メッセージのキーを返す。<br>
+     *
+     * @since 0.1.0
+     *
+     * @return メッセージのキー
+     *
+     * @see #getKey()
+     */
+    @Override
+    public String getCode() {
+
+        final String result = this.getKey();
+        return result;
+
+    }
+
+    /**
+     * 詳細情報を返す。<br>
+     *
+     * @since 0.1.0
+     *
+     * @return 詳細情報
+     */
+    @Override
+    public String getDetail() {
+
+        final String result = this.detail;
+        return result;
+
+    }
+
+    /**
+     * 表示名を返す。<br>
+     * <p>
+     * 識別するための表示名を返す。
+     * </p>
+     *
+     * @since 0.1.0
+     *
+     * @return 表示名
+     */
+    @Override
+    public String getDisplayName() {
+
+        final String result = this.displayName;
+        return result;
+
+    }
+
+    /**
+     * メッセージのキーを返す。<br>
+     *
+     * @since 0.1.0
+     *
+     * @return メッセージのキー
+     */
+    @Override
+    public String getKey() {
+
+        final String result = this.key;
+        return result;
+
+    }
+
+    /**
+     * メッセージの値を返す。
+     *
+     * @since 0.1.0
+     *
+     * @return メッセージの値
+     */
+    @Override
+    public String getValue() {
+
+        final String result = this.value;
+        return result;
+
+    }
+
+    /**
+     * メッセージのキーを返す。<br>
+     *
+     * @since 0.1.0
+     *
+     * @return メッセージのキー
+     *
+     * @see #getKey()
+     */
+    @Override
+    public String toString() {
+
+        final String result = this.getKey();
+        return result;
+
+    }
+
+}
---
+++ src/main/java/kmg/tool/infrastructure/common/KmgToolComValMessageTypes.java
@@ -0,0 +1,18 @@
+package kmg.tool.infrastructure.common;
+
+import kmg.core.infrastructure.common.KmgComValMessageTypes;
+
+/**
+ * KMG共通バリデーションメッセージの種類のインタフェース
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+public interface KmgToolComValMessageTypes extends KmgToolComMessageTypes, KmgComValMessageTypes {
+
+    // 処理なし
+
+}
