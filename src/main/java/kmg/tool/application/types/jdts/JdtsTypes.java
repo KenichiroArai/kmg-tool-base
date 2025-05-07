c6a080ad4e717ee570840c23d75ddfbf3997da16
KenichiroArai <sammyleejunior@gmail.com>
2025-05-07 23:36:27 +0900

    JdtsTypes列挙型を追加し、種類のマップと関連メソッドを実装

---
+++ src/main/java/kmg/tool/application/types/jdts/JdtsTypes.java
@@ -0,0 +1,227 @@
+package kmg.tool.application.types.jdts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import kmg.core.infrastructure.common.KmgComTypes;
+
+/**
+ * KMGテンプレートの種類<br>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+public enum JdtsTypes implements KmgComTypes<String> {
+
+    /* 定義：開始 */
+
+    /**
+     * 指定無し
+     *
+     * @author KenichiroArai
+     *
+     * @since 0.1.0
+     */
+    NONE("指定無し", "None", "指定無し"),
+
+    /* 定義：終了 */
+    ;
+
+    /**
+     * 種類のマップ
+     *
+     * @since 0.1.0
+     */
+    private static final Map<String, JdtsTypes> VALUES_MAP = new HashMap<>();
+
+    static {
+
+        /* 種類のマップにプット */
+        for (final JdtsTypes type : JdtsTypes.values()) {
+
+            JdtsTypes.VALUES_MAP.put(type.get(), type);
+
+        }
+
+    }
+
+    /**
+     * 表示名
+     *
+     * @since 0.1.0
+     */
+    private final String displayName;
+
+    /**
+     * キー
+     *
+     * @since 0.1.0
+     */
+    private final String key;
+
+    /**
+     * 詳細情報
+     *
+     * @since 0.1.0
+     */
+    private final String detail;
+
+    /**
+     * デフォルトの種類を返す<br>
+     *
+     * @since 0.1.0
+     *
+     * @return デフォルト値
+     */
+    public static JdtsTypes getDefault() {
+
+        final JdtsTypes result = NONE;
+        return result;
+
+    }
+
+    /**
+     * キーに該当する種類を返す<br>
+     * <p>
+     * 但し、キーが存在しない場合は、指定無し（NONE）を返す。
+     * </p>
+     *
+     * @since 0.1.0
+     *
+     * @param key
+     *            キー
+     *
+     * @return 種類。指定無し（NONE）：キーが存在しない場合。
+     */
+    public static JdtsTypes getEnum(final String key) {
+
+        JdtsTypes result = JdtsTypes.VALUES_MAP.get(key);
+
+        if (result == null) {
+
+            result = NONE;
+
+        }
+        return result;
+
+    }
+
+    /**
+     * 初期値の種類を返す<br>
+     *
+     * @since 0.1.0
+     *
+     * @return 初期値
+     */
+    public static JdtsTypes getInitValue() {
+
+        final JdtsTypes result = NONE;
+        return result;
+
+    }
+
+    /**
+     * コンストラクタ<br>
+     *
+     * @since 0.1.0
+     *
+     * @param displayName
+     *                    表示名
+     * @param key
+     *                    キー
+     * @param detail
+     *                    詳細情報
+     */
+    JdtsTypes(final String displayName, final String key, final String detail) {
+
+        this.displayName = displayName;
+        this.key = key;
+        this.detail = detail;
+
+    }
+
+    /**
+     * キーを返す。<br>
+     *
+     * @since 0.1.0
+     *
+     * @return キー
+     *
+     * @see #getKey()
+     */
+    @Override
+    public String get() {
+
+        final String result = this.getKey();
+        return result;
+
+    }
+
+    /**
+     * 詳細情報を返す。<br>
+     *
+     * @since 0.1.0
+     *
+     * @return 詳細情報
+     */
+    @Override
+    public String getDetail() {
+
+        final String result = this.detail;
+        return result;
+
+    }
+
+    /**
+     * 表示名を返す。<br>
+     * <p>
+     * 識別するための表示名を返す。
+     * </p>
+     *
+     * @since 0.1.0
+     *
+     * @return 表示名
+     */
+    @Override
+    public String getDisplayName() {
+
+        final String result = this.displayName;
+        return result;
+
+    }
+
+    /**
+     * キーを返す。<br>
+     *
+     * @since 0.1.0
+     *
+     * @return キー
+     */
+    @Override
+    public String getKey() {
+
+        final String result = this.key;
+        return result;
+
+    }
+
+    /**
+     * キーを返す。<br>
+     *
+     * @since 0.1.0
+     *
+     * @return キー
+     *
+     * @see #getKey()
+     */
+    @Override
+    public String toString() {
+
+        final String result = this.getKey();
+        return result;
+
+    }
+}
