76cf0c5036096682995a99655b7a4848afbec050
KenichiroArai <sammyleejunior@gmail.com>
2025-05-17 01:11:29 +0900

    Javadoc行削除ロジックインタフェースとその実装クラスを新規作成。未実装のTODOを追加。

---
+++ src/main/java/kmg/tool/application/logic/JavadocLineRemoverLogic.java
@@ -0,0 +1,12 @@
+package kmg.tool.application.logic;
+
+/**
+ * Javadoc行削除ロジックインタフェース<br>
+ *
+ * @author KenichiroArai
+ */
+public interface JavadocLineRemoverLogic {
+
+    // TODO KenichiroArai 2025/05/17 未実装
+
+}
---
+++ src/main/java/kmg/tool/application/logic/impl/JavadocLineRemoverLogicImpl.java
@@ -0,0 +1,17 @@
+package kmg.tool.application.logic.impl;
+
+import org.springframework.stereotype.Service;
+
+import kmg.tool.application.logic.JavadocLineRemoverLogic;
+
+/**
+ * Javadoc行削除サービス<br>
+ *
+ * @author KenichiroArai
+ */
+@Service
+public class JavadocLineRemoverLogicImpl implements JavadocLineRemoverLogic {
+
+    // TODO KenichiroArai 2025/05/17 未実装
+
+}
