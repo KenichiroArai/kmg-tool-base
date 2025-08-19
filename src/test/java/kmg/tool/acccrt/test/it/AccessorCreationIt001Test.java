a29c0a35cff9ea2e91856de82691f7988b196b98
KenichiroArai <sammyleejunior@gmail.com>
2025-08-20 00:22:13 +0900

    #67 結合テスト用のクラスを作成した。
    (Javadocタグ設定ツールでクラスのJavadocのタグが文の途中で追加されている #67)

---
+++ src/test/java/kmg/tool/acccrt/test/it/AccessorCreationIt001Test.java
@@ -0,0 +1,19 @@
+package kmg.tool.acccrt.test.it;
+
+import kmg.core.infrastructure.test.AbstractKmgTest;
+
+/**
+ * アクセサ作成の結合テスト001テスト<br>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+@SuppressWarnings({
+    "nls", "static-method"
+})
+public class AccessorCreationIt001Test extends AbstractKmgTest {
+    // TODO KenichiroArai 2025/08/20 未実装
+}
