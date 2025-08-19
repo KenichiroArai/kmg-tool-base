3ee6a3e24221e77fb701158dd86acf405aa71880
KenichiroArai <sammyleejunior@gmail.com>
2025-08-20 00:59:38 +0900

    #67 対象のファイルとテンプレートファイルを追加した。
    (Javadocタグ設定ツールでクラスのJavadocのタグが文の途中で追加されている #67)

--- src/test/java/kmg/tool/acccrt/test/it/AccessorCreationIt001Test.java
+++ src/test/java/kmg/tool/acccrt/test/it/AccessorCreationIt001Test.java
@@ -1,9 +1,18 @@
 package kmg.tool.acccrt.test.it;
 
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
 import kmg.core.infrastructure.test.AbstractKmgTest;
+import kmg.tool.acccrt.presentation.ul.cli.AccessorCreationTool;
 
 /**
- * アクセサ作成の結合テスト001テスト<br>
+ * アクセサ作成ツールの結合テスト001のテスト<br>
  *
  * @author KenichiroArai
  *
@@ -11,9 +20,77 @@ import kmg.core.infrastructure.test.AbstractKmgTest;
  *
  * @version 0.1.0
  */
+@SpringBootTest(classes = AccessorCreationTool.class)
+@TestPropertySource(properties = {
+    "spring.main.web-application-type=none"
+})
 @SuppressWarnings({
     "nls", "static-method"
 })
 public class AccessorCreationIt001Test extends AbstractKmgTest {
-    // TODO KenichiroArai 2025/08/20 未実装
+
+    /**
+     * テスト用の一時ディレクトリ
+     *
+     * @since 0.1.0
+     */
+    @TempDir
+    private Path tempDir;
+
+    /**
+     * 入力ファイルパス
+     *
+     * @since 0.1.0
+     */
+    private Path inputFilePath;
+
+    /**
+     * テンプレートファイルパス
+     *
+     * @since 0.1.0
+     */
+    private Path templateFilePath;
+
+    /**
+     * 各テスト実行前のセットアップ処理。テスト用ファイルの準備を行う。
+     *
+     * @since 0.1.0
+     *
+     * @throws Exception
+     *                   例外
+     */
+    @BeforeEach
+    public void setUp() throws Exception {
+
+        /* テスト用ファイルの準備 */
+        this.inputFilePath = this.tempDir.resolve("DtcDerivedPlaceholderModelImpl.java");
+        this.templateFilePath = this.tempDir.resolve("template.yml");
+
+    }
+
+    /**
+     * main メソッドの結合テスト - 正常系：実際のファイルを使用してツールが正常に動作する場合
+     *
+     * @since 0.1.0
+     *
+     * @throws Exception
+     *                   例外
+     */
+    @Test
+    public void testMain_integrationSuccess() throws Exception {
+
+        /* 期待値の定義 */
+
+        /* 準備 */
+        final String[] testArgs = {};
+
+        /* テスト対象の実行 */
+        AccessorCreationTool.main(testArgs);
+
+        /* 検証の準備 */
+
+        /* 検証の実施 */
+
+    }
+
 }
