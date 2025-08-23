7e0b99e1c63f76f731c6ae6607170aca26a32bdf
KenichiroArai <sammyleejunior@gmail.com>
2025-08-24 00:00:35 +0900

    #69 テストメソッドパスの取得を共通処理から取得するように修正した。
    (結合テストクラスのコードを整理と期待値との比較できるようにする #69)

--- src/test/java/kmg/tool/jdts/test/it/JavadocTagSetterIt001lTest.java
+++ src/test/java/kmg/tool/jdts/test/it/JavadocTagSetterIt001lTest.java
@@ -3,14 +3,13 @@ package kmg.tool.jdts.test.it;
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.io.TempDir;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import kmg.core.infrastructure.test.AbstractKmgTest;
-import kmg.core.infrastructure.type.KmgString;
-import kmg.core.infrastructure.utils.KmgPathUtils;
 import kmg.tool.jdts.application.service.impl.JdtsServiceImpl;
 import kmg.tool.jdts.presentation.ui.cli.JavadocTagSetterTool;
 
@@ -51,23 +50,26 @@ public class JavadocTagSetterIt001lTest extends AbstractKmgTest {
      *
      * @since 0.1.0
      *
+     * @param testInfo
+     *                 テスト情報
+     *
      * @throws Exception
      *                   例外
      */
     @Test
-    public void testMain_normal() throws Exception {
+    public void testMain_normal(final TestInfo testInfo) throws Exception {
 
         /* 期待値の定義 */
 
         /* 準備 */
-        final Path fqcnPath = KmgPathUtils.getFqcnPath(JavadocTagSetterIt001lTest.class);
-        final Path testPath = AbstractKmgTest.TEST_RESOURCES.resolve(fqcnPath);
 
-        final String testMethodName     = "testMain_normal";
-        final Path   testMethodNamePath = Path.of(KmgString.snakeCase(testMethodName));
-        final Path   testMethodPath     = testPath.resolve(testMethodNamePath);
+        // テストメソッドパス
+        final Path testMethodPath = this.getCurrentTestMethodPath(testInfo);
 
-        final Path testInputPath      = testMethodPath.resolve("TestInput.java");
+        // テスト入力ファイルパス
+        final Path testInputPath = testMethodPath.resolve("TestInput.java");
+
+        // テスト定義ファイルパス
         final Path testDefinitionPath = testMethodPath.resolve("TestTemplate.yml");
 
         /* テスト対象の実行 */
