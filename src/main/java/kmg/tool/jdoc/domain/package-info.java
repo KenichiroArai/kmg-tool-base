334f0a278b8a98b10332facae8164a7d084622b9
KenichiroArai <sammyleejunior@gmail.com>
2025-09-22 01:01:05 +0900

    #136 Javadoc処理パッケージの各package-info.javaを作成した。
    (package-infoを整備する #136)

---
+++ src/main/java/kmg/tool/jdoc/domain/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Javadoc処理のドメインパッケージ<br>
+ * <p>
+ * Javadoc処理機能のドメイン層を提供します。<br>
+ * モデル、型定義などのドメイン固有の実装が含まれます。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.jdoc.domain;
