8da7de04a2ae885b5c5d5036142cf0f7e81df774
KenichiroArai <sammyleejunior@gmail.com>
2025-09-22 01:07:07 +0900

    #136 2入力1出力パッケージの各package-info.javaを作成した。
    (package-infoを整備する #136)

---
+++ src/main/java/kmg/tool/two2one/presentation/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 2入力1出力のプレゼンテーションパッケージ<br>
+ * <p>
+ * 2入力1出力処理機能のプレゼンテーション層を提供します。<br>
+ * CLI、GUIなどのユーザーインターフェースの実装が含まれます。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.two2one.presentation;
