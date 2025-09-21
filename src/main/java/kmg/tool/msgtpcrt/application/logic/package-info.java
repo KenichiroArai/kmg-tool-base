d27da896fcc7930674fd8abd0a474e8bff2d89ad
KenichiroArai <sammyleejunior@gmail.com>
2025-09-22 01:05:20 +0900

    #136 メッセージタイプ作成パッケージの各package-info.javaを作成した。
    (package-infoを整備する #136)

---
+++ src/main/java/kmg/tool/msgtpcrt/application/logic/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * メッセージタイプ作成のロジックパッケージ<br>
+ * <p>
+ * メッセージタイプ作成機能のビジネスロジックを提供します。<br>
+ * メッセージタイプの解析、変換などのコア機能が実装されています。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.msgtpcrt.application.logic;
