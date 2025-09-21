c684dd0cd8a531a8349610b9a9472ccad68bc360
KenichiroArai <sammyleejunior@gmail.com>
2025-09-22 00:17:31 +0900

    #136 package-info.javaを作成した。
    (package-infoを整備する #136)

---
+++ src/main/java/kmg/tool/simple/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * シンプルのパッケージ<br>
+ * <p>
+ * シンプルは、サンプルとしての意味合いも持ち、ツール作成時の参考とすることができる。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.simple;
