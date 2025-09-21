bd9f5a2c9b29e65e13105d333f9b607eb7116085
KenichiroArai <sammyleejunior@gmail.com>
2025-09-22 00:58:15 +0900

    #136 入力→中間→出力パッケージの各package-info.javaを作成した。
    (package-infoを整備する #136)

---
+++ src/main/java/kmg/tool/iito/domain/service/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 入力→中間→出力のサービスパッケージ<br>
+ * <p>
+ * 入力→中間→出力処理機能のドメインサービスを提供します。<br>
+ * ロジック層とプレゼンテーション層を仲介するサービスの実装が含まれます。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.iito.domain.service;
