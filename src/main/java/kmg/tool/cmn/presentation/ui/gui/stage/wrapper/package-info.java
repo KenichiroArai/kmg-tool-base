234aa17c0e98122c8ddb86b8b55d61cc56dececc
KenichiroArai <sammyleejunior@gmail.com>
2025-09-22 00:43:37 +0900

    #136 共通パッケージの各package-info.javaを作成した。
    (package-infoを整備する #136)

---
+++ src/main/java/kmg/tool/cmn/presentation/ui/gui/stage/wrapper/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 共通のステージラッパーパッケージ<br>
+ * <p>
+ * KMGツール群で共通して使用されるステージラッパーを提供します。<br>
+ * ファイル選択ダイアログ、ディレクトリ選択ダイアログなどのラッパークラスが実装されています。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.cmn.presentation.ui.gui.stage.wrapper;
---
+++ src/main/java/kmg/tool/cmn/presentation/ui/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 共通のユーザーインターフェースパッケージ<br>
+ * <p>
+ * KMGツール群で共通して使用されるユーザーインターフェース層を提供します。<br>
+ * CLI、GUIなどのユーザーインターフェースの基盤となる実装が含まれます。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.cmn.presentation.ui;
