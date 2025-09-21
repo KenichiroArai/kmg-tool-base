b14f2e18de142982544845095385d0f01bc52eb4
KenichiroArai <sammyleejunior@gmail.com>
2025-09-22 00:57:34 +0900

    #136 インタフェースアクセサ作成パッケージの各package-info.javaを作成した。
    (package-infoを整備する #136)

---
+++ src/main/java/kmg/tool/ifacccrt/presentation/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * インタフェースアクセサ作成のプレゼンテーションパッケージ<br>
+ * <p>
+ * インタフェースアクセサ作成機能のプレゼンテーション層を提供します。<br>
+ * CLI、GUIなどのユーザーインターフェースの実装が含まれます。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.ifacccrt.presentation;
