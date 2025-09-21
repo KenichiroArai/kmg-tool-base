a0258ae5c740133cbfbe53851d9a1b9551b15ee0
KenichiroArai <sammyleejunior@gmail.com>
2025-09-22 01:02:39 +0900

    #136 Javadoc行削除パッケージの各package-info.javaを作成した。
    (package-infoを整備する #136)

---
+++ src/main/java/kmg/tool/jdocr/application/logic/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Javadoc行削除のロジックパッケージ<br>
+ * <p>
+ * Javadoc行削除機能のビジネスロジックを提供します。<br>
+ * ファイル解析、行削除などのコア機能が実装されています。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.jdocr.application.logic;
---
+++ src/main/java/kmg/tool/jdocr/application/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Javadoc行削除のアプリケーションパッケージ<br>
+ * <p>
+ * Javadoc行削除機能のアプリケーション層を提供します。<br>
+ * ロジック、サービス、型定義などのアプリケーション固有の実装が含まれます。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.jdocr.application;
