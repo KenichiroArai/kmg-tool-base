0eafe15fac961ebe56fab4a1a8b0a02a39f4684d
KenichiroArai <sammyleejunior@gmail.com>
2025-09-22 00:42:30 +0900

    #136 アクセサパッケージの各package-info.javaを作成した。
    (package-infoを整備する #136)

---
+++ src/main/java/kmg/tool/acccrt/application/types/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * アクセサ作成の型定義パッケージ<br>
+ * <p>
+ * アクセサ作成機能で使用される型定義を提供します。<br>
+ * 正規表現グループの種類などの列挙型が定義されています。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.acccrt.application.types;
