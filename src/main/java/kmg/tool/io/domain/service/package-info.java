ebdf1e4b7cadfb300374036ab16850e6a740e247
KenichiroArai <sammyleejunior@gmail.com>
2025-09-22 00:59:49 +0900

    #136 入出力処理パッケージの各package-info.javaを作成した。
    (package-infoを整備する #136)

---
+++ src/main/java/kmg/tool/io/domain/service/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * 入出力処理のサービスパッケージ<br>
+ * <p>
+ * 入出力処理機能のドメインサービスを提供します。<br>
+ * ファイルの入出力処理などのサービスの実装が含まれます。
+ * </p>
+ *
+ * @author KenichiroArai
+ *
+ * @since 0.1.0
+ *
+ * @version 0.1.0
+ */
+package kmg.tool.io.domain.service;
+
